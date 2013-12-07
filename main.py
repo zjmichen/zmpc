907babe8f073d98ef990ffdc9af510dd726e8104
Zack Michener <zack@zackmichener.net>
2013-12-06 22:27:20 -0800

    add mpd client support

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
+import os, re
 from gi.repository import Gtk
+from mpd import MPDClient
 
 class App:
 
@@ -11,7 +13,27 @@ class App:
 
         self.window = builder.get_object("window1")
 
+        mpd_host = os.getenv('MPD_HOST_TEST', 'localhost')
+        parts = re.split('@', mpd_host, 2)
+
+        if (len(parts) > 1):
+            self.mpd_pass = parts[0]
+            self.mpd_server = parts[1]
+        else:
+            self.mpd_pass = ''
+            self.mpd_server = parts[0]
+
+        self.mpd_port = os.getenv('MPD_PORT', 6600)
+
+        self.mpc = MPDClient()
+        self.mpc.timeout = 10
+
     def start(self):
+        self.mpc.connect(self.mpd_server, self.mpd_port)
+        if (len(self.mpd_pass) > 0):
+            self.mpc.password(self.mpd_pass)
+            
+        print(self.mpc.mpd_version)
         self.window.show_all()
 
 class Handler:
