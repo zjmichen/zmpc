dce84c21433475137bff8e0e93d4a55c176647ee
Zack Michener <zack@zackmichener.net>
2013-12-06 22:37:20 -0800

    add get song info

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ from mpd import MPDClient
 class App:
 
     def __init__(self):
-        builder = Gtk.Builder()
-        builder.add_from_file("main.glade")
-        builder.connect_signals(Handler())
+        self.builder = Gtk.Builder()
+        self.builder.add_from_file("main.glade")
+        self.builder.connect_signals(Handler())
 
-        self.window = builder.get_object("window1")
+        self.window = self.builder.get_object("window1")
 
         mpd_host = os.getenv('MPD_HOST_TEST', 'localhost')
         parts = re.split('@', mpd_host, 2)
@@ -32,9 +32,23 @@ class App:
         self.mpc.connect(self.mpd_server, self.mpd_port)
         if (len(self.mpd_pass) > 0):
             self.mpc.password(self.mpd_pass)
-            
+
+        self.update_info()
+
         print(self.mpc.mpd_version)
         self.window.show_all()
+
+    def update_info(self):
+        lbl_title = self.builder.get_object('lbl_title')
+        lbl_artist = self.builder.get_object('lbl_artist')
+        lbl_album = self.builder.get_object('lbl_album')
+
+        info = self.mpc.currentsong()
+
+        lbl_title.set_text(info['title'])
+        lbl_artist.set_text(info['albumartist'])
+        lbl_album.set_text(info['album'])
+
 
 class Handler:
     def onDeleteWindow(self, *args):
