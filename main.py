c8e334d34c308afb0bf4c6e2489063a3740a1108
Zack Michener <zack@zackmichener.net>
2013-12-08 02:30:24 -0800

    use named icons instead of deprecated stock icons

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import os, re, sys
+import os, re, sys, threading
 from gi.repository import Gtk, GLib, GObject
 from mpd import MPDClient
 
@@ -8,53 +8,53 @@ from nowplaying import NowPlaying
 from settings import Settings
 
 class App(Gtk.Application):
-    def __init__(self):
-        Gtk.Application.__init__(self, application_id='com.zackmichener.zmpc')
+  def __init__(self):
+    Gtk.Application.__init__(self, application_id='com.zackmichener.zmpc')
 
-        mpd_host = os.getenv('MPD_HOST', 'localhost')
-        parts = re.split('@', mpd_host, 2)
+    mpd_host = os.getenv('MPD_HOST', 'localhost')
+    parts = re.split('@', mpd_host, 2)
 
-        if (len(parts) > 1):
-            self.mpd_pass = parts[0]
-            self.mpd_server = parts[1]
-        else:
-            self.mpd_pass = ''
-            self.mpd_server = parts[0]
+    if (len(parts) > 1):
+      self.mpd_pass = parts[0]
+      self.mpd_server = parts[1]
+    else:
+      self.mpd_pass = ''
+      self.mpd_server = parts[0]
 
-        self.mpd_port = os.getenv('MPD_PORT', 6600)
+    self.mpd_port = os.getenv('MPD_PORT', 6600)
 
-        self.mpc = MPDClient()
-        self.mpc.timeout = 10
-        self.connect()
+    self.mpc = MPDClient()
+    self.mpc.timeout = 10
+    threading.Thread(target=self.connect).start()
 
-        self.lastfm_key = os.getenv('LASTFM_KEY', '')
-        self.lastfm_secret = os.getenv('LASTFM_SECRET', '')
+    self.lastfm_key = os.getenv('LASTFM_KEY', '')
+    self.lastfm_secret = os.getenv('LASTFM_SECRET', '')
 
-        self.cache_dir = os.path.join(GLib.get_user_cache_dir(), 'zmpc')
-        if not os.path.exists(self.cache_dir):
-            os.makedirs(self.cache_dir)
+    self.cache_dir = os.path.join(GLib.get_user_cache_dir(), 'zmpc')
+    if not os.path.exists(self.cache_dir):
+      os.makedirs(self.cache_dir)
 
-    def connect(self):
-        try:
-            self.mpc.connect(self.mpd_server, self.mpd_port)
-            if (len(self.mpd_pass) > 0):
-                self.mpc.password(self.mpd_pass)
-        except ConnectionRefusedError:
-            pass
+  def connect(self):
+    try:
+      self.mpc.connect(self.mpd_server, self.mpd_port)
+      if (len(self.mpd_pass) > 0):
+        self.mpc.password(self.mpd_pass)
+    except ConnectionRefusedError:
+      pass
 
-    def reconnect(self):
-        self.mpc.disconnect()
-        self.connect()
+  def reconnect(self):
+    self.mpc.disconnect()
+    self.connect()
 
-    def do_activate(self):
-        nowplaying = NowPlaying(self)
-        settings = Settings(self)
+  def do_activate(self):
+    nowplaying = NowPlaying(self)
+    #settings = Settings(self)
 
-    def do_startup(self):
-        Gtk.Application.do_startup(self)
+  def do_startup(self):
+    Gtk.Application.do_startup(self)
 
 if __name__ == "__main__":
-    GObject.threads_init()
-    app = App()
-    status = app.run(None)
-    sys.exit(status)
+  GObject.threads_init()
+  app = App()
+  status = app.run(None)
+  sys.exit(status)
--- nowplaying.py
+++ nowplaying.py
@@ -5,110 +5,110 @@ from xml.etree import ElementTree as ET
 from mpd import ConnectionError
 
 class NowPlaying:
-    UPDATE_INTERVAL = 1
-    playstate = 'pause'
+  UPDATE_INTERVAL = 1
+  playstate = 'pause'
 
-    def __init__(self, app):
-        self.app = app
-        self.builder = Gtk.Builder()
-        self.builder.add_from_file("nowplaying.glade")
-        self.builder.connect_signals(self)
+  def __init__(self, app):
+    self.app = app
+    self.builder = Gtk.Builder()
+    self.builder.add_from_file("nowplaying.glade")
+    self.builder.connect_signals(self)
 
-        self.window = self.builder.get_object("win_now_playing")
-        self.window.set_application(self.app)
+    self.window = self.builder.get_object("win_now_playing")
+    self.window.set_application(self.app)
 
-        self.update()
-        GObject.timeout_add(self.UPDATE_INTERVAL*1000, self.update)
+    self.update()
+    GObject.timeout_add(self.UPDATE_INTERVAL*1000, self.update)
 
-        self.window.show_all()
+    self.window.show_all()
 
-    def watch_status(self):
-        while(True):
-            self.update()
-            time.sleep(1)
+  def watch_status(self):
+    while(True):
+      self.update()
+      time.sleep(1)
 
-    def update(self):
-        t = threading.Thread(target=self.update_info)
-        t.daemon = True
-        t.start()
-        return True
+  def update(self):
+    t = threading.Thread(target=self.update_info)
+    t.daemon = True
+    t.start()
+    return True
 
-    def update_info(self):
-        try:
-          info = self.app.mpc.currentsong()
-          status = self.app.mpc.status()
-        except ConnectionError:
-          info = {
-            'title': 'Not Connected',
-            'artist': '',
-            'album': ''
-          }
-          status = { 'state': 'paused' }
+  def update_info(self):
+    try:
+      info = self.app.mpc.currentsong()
+      status = self.app.mpc.status()
+    except ConnectionError:
+      info = {
+      'title': 'Not Connected',
+      'artist': '',
+      'album': ''
+      }
+      status = { 'state': 'pause' }
 
-        lbl_title = self.builder.get_object('lbl_title')
-        lbl_title.set_text(info['title'])
-        lbl_artist = self.builder.get_object('lbl_artist')
-        lbl_artist.set_text(info['artist'])
-        lbl_album = self.builder.get_object('lbl_album')
-        lbl_album.set_text(info['album'])
+    lbl_title = self.builder.get_object('lbl_title')
+    lbl_title.set_text(info['title'])
+    lbl_artist = self.builder.get_object('lbl_artist')
+    lbl_artist.set_text(info['artist'])
+    lbl_album = self.builder.get_object('lbl_album')
+    lbl_album.set_text(info['album'])
 
-        if self.playstate != status['state']:
-            self.playstate = status['state']
-            btn_playpause = self.builder.get_object('btn_playpause')
-            if self.playstate == 'pause':
-                btn_playpause.set_stock_id('gtk-media-play')
-            else:
-                btn_playpause.set_stock_id('gtk-media-pause')
+    if self.playstate != status['state']:
+      self.playstate = status['state']
+      btn_playpause = self.builder.get_object('btn_playpause')
+      if self.playstate == 'pause':
+        btn_playpause.set_icon_name('media-playback-start')
+      else:
+        btn_playpause.set_icon_name('media-playback-pause')
 
-        self.update_cover(info)
+    self.update_cover(info)
 
-    def update_cover(self, info):
-        img_cover = self.builder.get_object('img_cover')
+  def update_cover(self, info):
+    img_cover = self.builder.get_object('img_cover')
 
-        if len(info['album']) > 0:
-          params = { 
-              'api_key': self.app.lastfm_key,
-              'method': 'album.getinfo'
-          }
+    if len(info['album']) > 0:
+      params = { 
+        'api_key': self.app.lastfm_key,
+        'method': 'album.getinfo'
+      }
 
-          params['artist'] = info['artist']
-          params['album'] = info['album']
+      params['artist'] = info['artist']
+      params['album'] = info['album']
 
-          url = 'http://ws.audioscrobbler.com/2.0/?'
-          for k in params:
-              url += k + '=' + params[k] + '&'
+      url = 'http://ws.audioscrobbler.com/2.0/?'
+      for k in params:
+        url += k + '=' + params[k] + '&'
 
-          res = requests.get(url)
-          xml = ET.fromstring(res.content.decode())
-          cover_url = xml.find("*image[@size='extralarge']")
-          if cover_url != None:
-              img_res = self.fetch_image(cover_url.text)
-              img_cover.set_from_pixbuf(img_res)
-        else:
-          img_cover.set_from_stock('gtk-missing-image', Gtk.IconSize.BUTTON)
+      res = requests.get(url)
+      xml = ET.fromstring(res.content.decode())
+      cover_url = xml.find("*image[@size='extralarge']")
+      if cover_url != None:
+        img_res = self.fetch_image(cover_url.text)
+        img_cover.set_from_pixbuf(img_res)
+    else:
+      img_cover.set_from_icon_name('gtk-missing-image', Gtk.IconSize.BUTTON)
 
-    def fetch_image(self, url):
-        basename = url.split("/")[-1]
-        fname = os.path.join(self.app.cache_dir, basename)
+  def fetch_image(self, url):
+    basename = url.split("/")[-1]
+    fname = os.path.join(self.app.cache_dir, basename)
 
-        if not os.path.exists(fname):
-            res = requests.get(url)
-            f = open(fname, "wb")
-            f.write(res.content)
-            f.close()
-            res.close()
+    if not os.path.exists(fname):
+      res = requests.get(url)
+      f = open(fname, "wb")
+      f.write(res.content)
+      f.close()
+      res.close()
 
-        return Pixbuf.new_from_file(fname)
+    return Pixbuf.new_from_file(fname)
 
-    def on_btn_previous_clicked(self, *args):
-        self.app.mpc.previous()
-        self.update()
+  def on_btn_previous_clicked(self, *args):
+    self.app.mpc.previous()
+    self.update()
 
-    def on_btn_playpause_clicked(self, *args):
-        self.app.mpc.pause()
-        self.update()
+  def on_btn_playpause_clicked(self, *args):
+    self.app.mpc.pause()
+    self.update()
 
-    def on_btn_next_clicked(self, *args):
-        self.app.mpc.next()
-        self.update()
+  def on_btn_next_clicked(self, *args):
+    self.app.mpc.next()
+    self.update()
 
