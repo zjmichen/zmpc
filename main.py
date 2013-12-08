73d0c29d5c403f6d247a8806da1f80cb5d32a4eb
Zack Michener <zack@zackmichener.net>
2013-12-08 03:22:01 -0800

    add app menu

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import os, re, sys, threading
-from gi.repository import Gtk, GLib, GObject
+from gi.repository import Gtk, GLib, GObject, Gio
 from mpd import MPDClient
 
 from nowplaying import NowPlaying
@@ -9,7 +9,25 @@ from settings import Settings
 
 class App(Gtk.Application):
   def __init__(self):
-    Gtk.Application.__init__(self, application_id='com.zackmichener.zmpc')
+    Gtk.Application.__init__(self, application_id='com.zackmichener.zmpc', register_session=True)
+
+  def connect(self):
+    try:
+      self.mpc.connect(self.mpd_server, self.mpd_port)
+      if (len(self.mpd_pass) > 0):
+        self.mpc.password(self.mpd_pass)
+    except ConnectionRefusedError:
+      pass
+
+  def reconnect(self):
+    self.mpc.disconnect()
+    self.connect()
+
+  def do_activate(self):
+    nowplaying = NowPlaying(self)
+
+  def do_startup(self):
+    Gtk.Application.do_startup(self)
 
     mpd_host = os.getenv('MPD_HOST', 'localhost')
     parts = re.split('@', mpd_host, 2)
@@ -34,24 +52,27 @@ class App(Gtk.Application):
     if not os.path.exists(self.cache_dir):
       os.makedirs(self.cache_dir)
 
-  def connect(self):
-    try:
-      self.mpc.connect(self.mpd_server, self.mpd_port)
-      if (len(self.mpd_pass) > 0):
-        self.mpc.password(self.mpd_pass)
-    except ConnectionRefusedError:
-      pass
+    self.create_menu()
 
-  def reconnect(self):
-    self.mpc.disconnect()
-    self.connect()
+  def create_menu(self):
+    menu = Gio.Menu()
+    menu.append("Settings", "app.settings")
+    menu.append("Quit", "app.quit")
+    self.set_app_menu(menu)
 
-  def do_activate(self):
-    nowplaying = NowPlaying(self)
-    #settings = Settings(self)
+    settings_action = Gio.SimpleAction.new("settings", None)
+    settings_action.connect("activate", self.menu_settings)
+    self.add_action(settings_action)
 
-  def do_startup(self):
-    Gtk.Application.do_startup(self)
+    quit_action = Gio.SimpleAction.new("quit", None)
+    quit_action.connect("activate", self.menu_quit)
+    self.add_action(quit_action)
+
+  def menu_settings(self, action, parameter):
+    settings = Settings(self)
+
+  def menu_quit(self, action, parameter):
+    self.quit()
 
 if __name__ == "__main__":
   GObject.threads_init()
