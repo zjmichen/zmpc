316816a9e410ceed9f1b1192f08784bfa438c494
Zack Michener <zackm@accretivetg.com>
2013-12-11 12:59:04 -0800

    fix streaming

--- stream.py
+++ stream.py
@@ -1,33 +1,21 @@
-from gi.repository import Gst
+from gi.repository import GObject, Gst
 
 Gst.init(None)
 
 class Stream:
   def __init__(self, host, port):
     self.streaming = False
+    self.pipe = self.getPipeline(host, port)
 
-    self.pipe = Gst.Pipeline()
-    tcpsrc = Gst.ElementFactory.make("tcpserversrc", "source")
-    self.pipe.add(tcpsrc)
-    tcpsrc.set_property("host", host)
-    tcpsrc.set_property("port", port)
-
-    decode = Gst.ElementFactory.make("decodebin", "decode")
-    decode.connect("pad-added", self.new_decode_pad)
-    self.pipe.add(decode)
-    tcpsrc.link(decode)
-
-    self.convert = Gst.ElementFactory.make("audioconvert", "convert")
-    self.pipe.add(self.convert)
-
-    sink = Gst.ElementFactory.make("alsasink", "sink")
-    self.pipe.add(sink)
-    self.convert.link(sink)
-
-  def new_decode_pad(self, dbin, pad, islast):
-    pad.link(self.convert.get_pad("sink"))
+  def getPipeline(self, host, port):
+    pipe = Gst.Pipeline()
+    playbin = Gst.ElementFactory.make("playbin", None)
+    pipe.add(playbin)
+    playbin.set_property('uri', 'http://' + host + ':' + str(port))
+    return pipe
 
   def start(self):
+    print("Starting stream...")
     self.pipe.set_state(Gst.State.PLAYING)
     self.streaming = True
 
